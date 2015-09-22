37190fa574b1fff1a0536e7e5d57c7ca581077c9
Jeong-Gyu Kim <omyojj@gmail.com>
2015-09-22 20:56:04 +0900

    Added an ipython startup file in ipython_linux

---
+++ ipython_linux/.ipython/profile_default/startup/00-first.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os,sys,pprint,glob
+
+lunit		= 3.085678e18	# length unit [pc]
+tunit		= 3.15576e13	# time unit   [Myr]
+dunit		= 2.3416704878e-24 # density unit in cgs [g/cm^3/H]
