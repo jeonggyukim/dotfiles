e2cedd070a8795f7104ca6615831544ef78b6982
Jeong-Gyu Kim <omyojj@gmail.com>
2017-02-21 15:33:10 +0900

    Further emacs settings on mac.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -20,13 +20,31 @@
 
 (package-initialize) ; initialize package.el
 
-;; (eval-when-compile
-;;   (require 'use-package))
-;; (require 'diminish)                ;; if you use :diminish
-;; (require 'bind-key)                ;; if you use any :bind variant
+(eval-when-compile
+  (require 'use-package))
+(require 'diminish)                ;; if you use :diminish
+(require 'bind-key)                ;; if you use any :bind variant
 
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
 
+(setq use-package-verbose t)
+(setq use-package-always-ensure t)
+(setq vc-follow-symlinks t)
+
 (org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "9d91458c4ad7c74cf946bd97ad085c0f6a40c370ac0a1cbeb2e3879f15b40553" "43c1a8090ed19ab3c0b1490ce412f78f157d69a29828aa977dae941b994b4147" "6f2e670f8af1338b0f1d01afcfc962d30e5b2a851989fc547d06e405ce7e24ed" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
